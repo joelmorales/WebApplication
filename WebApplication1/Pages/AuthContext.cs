6e51b51efbb237eb75148928624c3875b6b133b4
joelmorales <44100954+joelmorales@users.noreply.github.com>
2020-03-16 14:15:34 -0500

    Initial Commit

---
+++ WebApplication1/Pages/AuthContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace WebApplication1.Pages
+{
+    internal class AuthContext: DbContext
+    {
+        public DbSet<User> Users { get; internal set; }
+    }
+}
---
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AuthContext _context;
+
+        IndexModel(AuthContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void Redirect(string url)
+        {
+            RedirectPermanent(url);
+        }
+
+        // GET /SqlInjection/Authenticate
+        public IActionResult Authenticate(string user)
+        {
+            var query = "SELECT * FROM Users WHERE Username = '" + user + "'"; // Unsafe
+            var userExists = _context.Users.FromSql(query).Any(); // Noncompliant
+
+            // An attacker can bypass authentication by setting user to this special value
+            user = "' or 1=1 or ''='";
+
+            return Content(userExists ? "success" : "fail");
+        }
+    }
+}
